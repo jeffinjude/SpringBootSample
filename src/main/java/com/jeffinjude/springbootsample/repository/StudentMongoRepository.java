aacfbc9063e6a467496ed984b85d21e77ee976dc
592675 <592675@PC308393.cts.com>
2016-12-02 14:24:13 +0530

    mongo repository is not needed since mongo template is used

--- src/main/java/com/jeffinjude/springbootsample/repository/StudentMongoRepository.java
+++ src/main/java/com/jeffinjude/springbootsample/repository/StudentMongoRepository.java
@@ -3,7 +3,7 @@ package com.jeffinjude.springbootsample.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.jeffinjude.springbootsample.entity.Student;
-
+//NOTE: Since mongo template is used MongoRepository is not needed
 public interface StudentMongoRepository extends MongoRepository<Student, String> {
 
 }
